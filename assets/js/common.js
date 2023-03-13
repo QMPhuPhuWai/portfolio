0b164fbf2851e026f662588f87f10c63668c8f53
QMPhuPhuWai <phuphuwai@qualymm.com>
2023-03-13 23:16:57 +0630

    finish file

--- assets/js/common.js
+++ assets/js/common.js
@@ -23,3 +23,33 @@ $(".skill").waypoint(
   },
   { offset: "80%" }
 );
+
+/* =========================================
+ *  Menu
+ * ========================================= */
+const menu = document.querySelector(".menu");
+const lineOne = document.querySelector(".line1");
+const lineTwo = document.querySelector(".line2");
+const lineThree = document.querySelector(".line3");
+const Navbar = document.querySelector(".navi-div");
+const Body = document.querySelector("body");
+const NavItems = Array.from(document.querySelectorAll(".navi li"));
+menu.addEventListener("click", function () {
+  lineOne.classList.toggle("rotate-left");
+  lineTwo.classList.toggle("hide");
+  lineThree.classList.toggle("rotate-right");
+  Navbar.classList.toggle("sp_menu_open");
+  Body.classList.toggle("overflow-hide ");
+  $(".wrapper").toggle("active");
+});
+
+NavItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    lineOne.classList.remove("rotate-left");
+    lineTwo.classList.remove("hide");
+    lineThree.classList.remove("rotate-right");
+    Navbar.classList.remove("sp_menu_open");
+    Body.classList.remove("overflow-hide ");
+    $(".wrapper").remove("active");
+  });
+});
